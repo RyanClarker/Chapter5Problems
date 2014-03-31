8209ecaf10839821b3925d62247a21aaaccdb7ab
RyanClarker <clarke.ryan24@gmail.com>
2014-03-31 10:11:16 -0400

    new

--- ConsoleApplication1/ConsoleApplication3/Program.cs
+++ ConsoleApplication1/ConsoleApplication3/Program.cs
@@ -9,58 +9,26 @@ namespace ConsoleApplication3
     {
         static void Main(string[] args)
         {
-            int[] zip = new int[10];
-            Console.WriteLine("Enter 10 Zip Codes between 1-1000");
+            
+            int[] zip = new int[10] { 50535, 30523, 59201, 10234, 02934, 92855, 20203, 72045, 30919, 40404 };
+            float[] charge = new float[10] { 0.10f, 0.20f, 0.30f, 0.40f, 0.50f, 0.60f, 0.70f, 0.80f, 0.90f, 1.0f };
+
+            Console.WriteLine("Enter your zip (61235, 30523, 59245, 10234, 02934, 92855, 51235, 72045, 30919, 40404):");
+            int zipinput = Convert.ToInt32(Console.ReadLine());
+
             for (int i = 0; i < zip.Length; i++)
             {
-                zip[i] = Convert.ToInt32(Console.ReadLine());
-                float[] charge = new float[10] { 0.10f, 0.20f, 0.30f, 0.40f, 0.50f, 0.60f, 0.70f, 0.80f, 0.90f, 1.0f };
-                if (zip[i] >= 0 && zip[i] < 50)
+                if (zipinput == zip[i])
                 {
-                    Console.WriteLine("The cost to deliver to your location is ${0}", charge[0]);
-                }
-                if (zip[i] >= 50 && zip[i] < 100)
-                {
-                    Console.WriteLine("The cost to deliver to your location is ${0}", charge[1]);
-                }
-                if (zip[i] >= 100 && zip[i] < 150)
-                {
-                    Console.WriteLine("The cost to deliver to your location is ${0}", charge[2]);
-                }
-                if (zip[i] >= 150 && zip[i] < 200)
-                {
-                    Console.WriteLine("The cost to deliver to your location is ${0}", charge[3]);
-                }
-                if (zip[i] >= 200 && zip[i] < 250)
-                {
-                    Console.WriteLine("The cost to deliver to your location is ${0}", charge[4]);
-                }
-                if (zip[i] >= 250 && zip[i] < 300)
-                {
-                    Console.WriteLine("The cost to deliver to your location is ${0}", charge[5]);
-                }
-                if (zip[i] >= 300 && zip[i] < 350)
-                {
-                    Console.WriteLine("The cost to deliver to your location is ${0}", charge[6]);
-                }
-                if (zip[i] >= 350 && zip[i] < 400)
-                {
-                    Console.WriteLine("The cost to deliver to your location is ${0}", charge[7]);
-                }
-                if (zip[i] >= 400 && zip[i] < 450)
-                {
-                    Console.WriteLine("The cost to deliver to your location is ${0}", charge[8]);
-                }
-                if (zip[i] >= 450 && zip[i] < 500)
-                {
-                    Console.WriteLine("The cost to deliver to your location is ${0}", charge[9]);
+                    Console.WriteLine("The cost to deliver to your house is ${0}!", charge[i]);
+                    
                 }
 
-                if (zip[i] > 501)
+                else
                 {
-                    Console.WriteLine("Your location is too far to deliver");
+                    Console.WriteLine("We don't deliver there.");
+                    
                 }
-
             }
         }
     }
---
+++ ConsoleApplication1/ConsoleApplication5/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double minutes, totalcost = 0;
+            int[] area = new int[6] { 262, 414, 608, 715, 815, 920 };
+            double[] cost = new double[6] { 0.07d, 0.10d, 0.05d, 0.16d, 0.24d, 0.14d };
+            bool isareainput = false;
+            Console.WriteLine("Enter your area code (262, 414, 608, 715, 815, 920):");
+            int areainput = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter the minutes that you've used.");
+             minutes = Convert.ToInt32(Console.ReadLine());
+            
+            
+
+            for (int i = 0; i <= area.Length; i++)
+            {
+                if (areainput == area[i])
+                {
+                    isareainput = true;
+                    totalcost = cost[i];
+                }
+                if (isareainput)
+                {
+                    Console.WriteLine("The cost is ${0}", totalcost)
+            }
+        }
+    }
+}
